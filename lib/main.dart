08e8c0d93a4377439773ba3783969795edce66c6
AdjoaJunior <estagyemang7804@gmail.com>
2022-02-23 10:23:43 +0000

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,14 @@ import 'package:flutter/material.dart';
 import 'auth/decision.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
+   runApp(const MyApp());
 }
 
-void initialization() async {
-  // This is where you can initialize the resources needed by your app while
-  // the splash screen is displayed.  Remove the following example because
-  // delaying the user experience is a bad design practice!
-  // ignore_for_file: avoid_print
-  print('ready in 3...');
-  await Future.delayed(const Duration(seconds: 1));
-  print('ready in 2...');
-  await Future.delayed(const Duration(seconds: 1));
-  print('ready in 1...');
-  await Future.delayed(const Duration(seconds: 1));
-  print('go!');
-  //  FlutterNativeSplash.remove();
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(milliseconds: 3));
 }
 
 class MyApp extends StatelessWidget {
