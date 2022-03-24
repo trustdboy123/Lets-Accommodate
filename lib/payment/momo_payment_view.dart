83c69e124859fe5353b4f5a71faf0269024c7767
Ayiks <cliffysarp@gmail.com>
2022-03-24 20:03:40 +0000

    payment implementation

---
+++ lib/payment/momo_payment_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MomoPaymentView extends StatelessWidget {
+  const MomoPaymentView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/payment/paystack_provider.dart
@@ -0,0 +1,119 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_paystack/flutter_paystack.dart';
+
+enum WalletProvider {
+  mtn,
+  vod,
+  tgo,
+}
+
+class PaystackProvider {
+  // TODO: Insert your heroku url of your paystack backend server here without a trailing slash
+  String paystackBackendServerURL = "";
+
+  payWithMobileMoney({
+    required String email,
+    required String username,
+    required double amountInPesswas,
+    required String walletPhoneNumber,
+    required WalletProvider walletProvider,
+  }) async {
+    Dio dio = Dio();
+    try {
+      var response = await dio.post(
+        '$paystackBackendServerURL/payments/charge',
+        data: {
+          "email": email,
+          "amount": amountInPesswas,
+          "device_id": username,
+          "mobile_money": {
+            "phone": walletPhoneNumber,
+            "provider": walletProvider.name
+          }
+        },
+      );
+      if (response.statusCode == 200 &&
+          response.data["data"]["status"] == "send_otp") {
+        print("Charge response: ${response.data}");
+        // TODO: README
+        // the user needs to enter an OTP code sent from Paystack to authorize the payment
+        // so navigate the user to the screen/modal dialog where they can enter the otp code and submit it
+        // pass the `walletPhoneNumber`,  `response.data["data"]["reference"]`, and the `sendOtpRequest` function as required props  to the screen/dialog/modal
+        // when submitted, call the `sendOtpRequest` function pass in the otp entered by the user and the reference `response.data["data"]["reference"]`
+        // to finalize the payment
+      }
+    } on DioError catch (error) {
+      handleError(error);
+    }
+
+    dio.close();
+  }
+
+  sendOtpRequest({required String otp, required String reference}) async {
+    print("Sending OTP request: $otp , with reference: $reference");
+    Dio dio = Dio();
+    try {
+      var response = await dio.post(
+        '$paystackBackendServerURL/payments/submit-otp',
+        data: {
+          "otp": otp,
+          "reference": reference,
+        },
+      );
+      if (response.statusCode == 200) {
+        print("Sent OTP response: ${response.data}");
+        // show a toast with `response.data["data"]["display_text"]`
+      }
+    } on DioError catch (error) {
+      handleError(error);
+    }
+    dio.close();
+  }
+
+  payWithCreditCard({
+    required PaystackPlugin paystackPlugin,
+    required BuildContext context,
+    amount,
+    email,
+  }) async {
+    Dio dio = Dio();
+    try {
+      var response = await dio.post(
+          '$paystackBackendServerURL/payments/generate-access-token',
+          data: {
+            "email": email,
+            "amount": amount,
+          });
+      if (response.statusCode == 200) {
+        print("Access Code response: ${response.data["data"]["access_code"]}");
+
+        Charge charge = Charge()
+          ..email = email
+          ..amount = 1 * 100
+          ..currency = "GHS"
+          ..accessCode = response.data["data"]["access_code"];
+        // ..reference = _getReference() //TODO: Get access_token from backend
+        CheckoutResponse checkoutResponse = await paystackPlugin.checkout(
+          context,
+          charge: charge,
+          method: CheckoutMethod.card, // Defaults to CheckoutMethod.selectable
+        );
+        print(checkoutResponse);
+      }
+    } on DioError catch (error) {
+      handleError(error);
+    }
+    dio.close();
+  }
+
+  /// prints and toast an error
+  void handleError(DioError error) {
+    if (error.response != null) {
+      print(error);
+      print(error.response?.data);
+    } else {
+      print(error.message);
+    }
+  }
+}
